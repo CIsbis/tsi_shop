321de287908f2311c74afa7e4579039cdca49b88
Cameron Isbister <2955880I@student.gla.ac.uk>
2024-04-18 15:09:31 +0100

    Mocks for Addcar and SignIn

--- src/AddCar_mock.py
+++ src/AddCar_mock.py
@@ -20,3 +20,4 @@ class TestAddCar(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
--- src/SignIn_mock.py
+++ src/SignIn_mock.py
@@ -22,3 +22,5 @@ class TestSignUp(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
