cd2a1f9c5e8a9bbaa938a20027f99d4c5df556d8
unknown <2889991R@student.gla.ac.uk>
2024-04-21 14:57:13 +0100

    Fixed the filter so you can have a more specific search

--- src/SerchCar.py
+++ src/SerchCar.py
@@ -1,14 +1,14 @@
 from ReadCSVFile import ReadCSVFile
 
 
-def search_car_logic(filters):
+def search_car_logic(filter_list):
     found_cars = ReadCSVFile().get_file_data('cars.csv')
     filtered_cars = []
 
     for row in found_cars:
-        if row:
-            if any(filter_func(row) for filter_func in filters):
-                filtered_cars.append(row)
+        if row and check_if_filters_match(row, filter_list):
+            filtered_cars.append(row)
+
     return filtered_cars
 
 
@@ -16,32 +16,35 @@ def make_filter_functions(filters):
     filter_functions = []
     for filter_name, filter_value in filters:
         if filter_name == 'make':
-            filter_functions.append(lambda row: row[0].strip().lower() == filter_value.strip().lower())
-        elif filter_name == 'color':
-            filter_functions.append(lambda row: row[2].strip().lower() == filter_value.strip().lower())
+            filter_functions.append(('make', filter_value.strip().lower()))
         elif filter_name == 'model':
-            filter_functions.append(lambda row: row[1].strip().lower() == filter_value.strip().lower())
-        elif filter_name == 'automatic_or_manual':
-            filter_functions.append(lambda row: row[5].strip().lower() == filter_value.strip().lower())
-        elif filter_name == 'electric_or_not':
-            filter_functions.append(lambda row: row[7].strip().lower() == filter_value.strip().lower())
-        elif filter_name == 'mileage':
-            filter_functions.append(lambda row: row[4].strip().lower() == filter_value.strip().lower())
+            filter_functions.append(('model', filter_value.strip().lower()))
+        elif filter_name == 'colour':
+            filter_functions.append(('colour', filter_value.strip().lower()))
         elif filter_name == 'year':
-            filter_functions.append(lambda row: row[3].strip().lower() == filter_value.strip().lower())
+            filter_functions.append(('year', filter_value.strip().lower()))
+        elif filter_name == 'mileage':
+            filter_functions.append(('mileage', filter_value.strip().lower()))
+        elif filter_name == 'automatic_or_manual':
+            filter_functions.append(('automatic_or_manual', filter_value.strip().lower()))
+        elif filter_name == 'electric_or_not':
+            filter_functions.append(('electric_or_not', filter_value.strip().lower()))
     return filter_functions
 
 
 def search_cars():
     filters = []
     filters.append(('make', input("Enter the make of the car you want to search for (leave empty to skip): ").strip()))
-    filters.append(('color', input("Enter the color of the car you want to search for (leave empty to skip): ").strip()))
-    filters.append(('model', input("Enter the model of the car you want to search for (leave empty to skip): ").strip()))
+    filters.append(
+        ('colour', input("Enter the colour of the car you want to search for (leave empty to skip): ").strip()))
+    filters.append(
+        ('model', input("Enter the model of the car you want to search for (leave empty to skip): ").strip()))
     filters.append(('automatic_or_manual', input("Enter if you want the search to show you automatic or manual cars "
                                                  "(leave empty to skip): ").strip()))
     filters.append(('electric_or_not', input("Enter if you want the search to show you electric, petrol or diesel cars"
                                              " (leave empty to skip): ").strip()))
-    filters.append(('mileage', input("Enter the mileage of the car you want to search for (leave empty to skip): ").strip()))
+    filters.append(
+        ('mileage', input("Enter the mileage of the car you want to search for (leave empty to skip): ").strip()))
     filters.append(('year', input("Enter the year of the car you want to search for (leave empty to skip): ").strip()))
 
     filters = [(name, value) for name, value in filters if value]
@@ -60,3 +63,13 @@ def search_cars():
             print(car)
     else:
         print("No cars found.")
+
+
+def check_if_filters_match(row, filters):
+    row_lower = list(map(lambda i: i.lower(), row))
+    for filter_name, filter_value in filters:
+        if filter_value not in row_lower:
+            return False
+    return True
+
+search_cars()
